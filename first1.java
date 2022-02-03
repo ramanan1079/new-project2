b7cceacc2d9119201d92094828cfe165ee1d7aa1
ramanan1079 <ramanan3067@gamil.com>
2022-02-03 11:37:33 +0530

    today new project2

---
+++ first1.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.Scanner;
+
+class Kavin{
+	public static void main(String args[]){
+	int m1;
+	Scanner s =new Scanner(System.in);
+	System.out.println("enter the marks : " + m1);
+	int marks=s.nextint; 
+}
+}
